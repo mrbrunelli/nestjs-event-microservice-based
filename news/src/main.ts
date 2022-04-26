a455d2b5f4a142c0c177c2876ef0c1584cd1af62
mrbrunelli <matheus.brunelli@gmail.com>
2022-04-26 18:12:22 -0300

    remove unused config

--- news/src/main.ts
+++ news/src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.TCP,
-  //   options: {
-  //     host: '0.0.0.0',
-  //     port: 5000,
-  //   },
-  // });
-
-  // await app.startAllMicroservices();
-
   await app.listen(3000);
 }
 bootstrap();
